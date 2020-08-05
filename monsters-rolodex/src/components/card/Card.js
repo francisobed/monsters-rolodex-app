29f21e6947129fd6074a16134f5f593294f29379
francisobed <francisobadiahajigana@gmail..com>
2020-08-05 17:03:06 +0100

    adding files for github pages

---
+++ monsters-rolodex/src/components/card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+
+export const Card = props => (
+        <div className='card-container'>
+        <img 
+        alt='monster' 
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        />
+         <h3>{props.monster.name}</h3>
+         <p>{props.monster.email}</p>
+        </div>
+    );
